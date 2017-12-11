f289a11db3b55cb03bb00cbd5bb82a2a982cab2b
VizzraN <Luke_Courtney@davincischools.org>
2017-12-11 13:28:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+   function changeScreenSize(){
+      window.resizeTo(375,667);
+   }
+
+   function PlaySound() {
+   var sound = document.getElementById("audiognar");sound.play()
+   }
+
+   function PlaySoundone() {
+   var sound = document.getElementById("audiozed");sound.play()
+   }
+
+   function PlaySoundtwo() {
+   var sound = document.getElementById("audioashe");sound.play()
+   }
+
+   function PlaySoundthree() {
+   var sound = document.getElementById("audiocaitlyn");sound.play()
+   }
+
+   function PlaySoundfour() {
+   var sound = document.getElementById("audioannie");sound.play()
+   }
+
+   function PlaySoundt() {
+   var sound = document.getElementById("audiotristana");sound.play()
+   }
